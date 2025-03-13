bdd197764f88d62727eac51285a494c408e430f1
Siphelele01 <siphelelegwabavu@gmail.com>
2025-03-13 15:15:33 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio - Siphelele Gwabavu</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <h1>Portfolio</h1>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#services">Projects</a></li>
+                <li><a href="#portfolio">Skills</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section id="home" class="hero">
+        <div class="content">
+            <h3>Hello, It's Me</h3>
+            <h2>Siphelele Gwabavu</h2>
+            <p>And I'm a <span class="highlight">Full-Stack Developer</span></p>
+            <p>Passionate about creating dynamic and responsive web applications.</p>
+        </div>
+        <div class="profile-image">
+            <img src="https://media.licdn.com/dms/image/v2/D5603AQEV-cw0SWDSaw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1719500597318?e=1747267200&v=beta&t=R0dDM-S3EKSnF_S-AVMCnCJm9oqyAG1JY2VP6W4dozU" alt="Siphelele Gwabavu">
+        </div>
+        </section>
+</body>
+</html>
